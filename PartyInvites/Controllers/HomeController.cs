2798b38fba45d6af59b7319f0276e0fbf1913f8d
makun <makunhappy@gmail.com>
2018-09-30 08:10:34 +0800

    add

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult  Index()
+        {
+            ViewBag.header = "My Index";
+            return View("MyView");
+            //return new RedirectResult("Home/StringStr");
+            //return new UnauthorizedResult();
+        }
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            //to do
+            return View();
+        }
+    }
+}
